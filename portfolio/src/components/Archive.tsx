849063c4096a375ccb19dd4fd4e1c97cd053f173
SzymCode <s.radomski19@gmail.com>
2023-08-22 22:05:37 +0200

    Project list updated + display changes

--- portfolio/src/components/Archive.tsx
+++ portfolio/src/components/Archive.tsx
@@ -15,7 +15,7 @@ export default function Archive() {
         initial={{ y: 5, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.2, delay: 0.4 }}
-        className="max-w-contentContainer mx-auto px-2 py-10 md:py-32"
+        className="max-w-container mx-auto py-10 md:py-32"
       >
         <div className="w-full flex flex-col items-center">
           <h2 className="text-3xl font-semibold">
@@ -37,6 +37,7 @@ export default function Archive() {
                 title={cardData.title}
                 description={cardData.description}
                 listItem={cardData.listItem}
+                githubLink={cardData.githubLink}
                 link={cardData.link}/>
             </motion.div>
           ))}
@@ -58,68 +59,94 @@ const archiveCardsData: ArchiveCardProps[] = [
     description:
       "This website helps storage contacts data and share it with others. This repository serves as my coding sandbox, where I can experiment with different solutions and refine my skills.",
     listItem: ["React", "Django", "TypeScript", "Tailwind"],
-    link: "https://github.com/SzymCode/ContactBook"
+    githubLink: "https://github.com/SzymCode/ContactBook",
+    link: "https://contactbook-szymcode.herokuapp.com/lists"
+  },
+  {
+    title: "GiardDesign",
+    description:
+      "Fully responsive website with photo gallery, made for adRespect company recruitment process. Graphic design is their own.",
+    listItem: ["HTML", "Javascript", "Tailwind", "Bootstrap"],
+    githubLink: "https://github.com/SzymCode/RecruitmentTasks/tree/giard-design",
+    link: "https://szymcode.github.io/RecruitmentTasks/"
   },
   {
     title: "ECommerceTemplate",
     description:
       "Store initialization template with all main functionalities. Includes essential features such as product listings, shopping cart and checkout processes connected with Stripe.",
     listItem: ["React", "Next.js", "Strapi", "Tailwind"],
-    link: "https://github.com/SzymCode/ECommerceTemplate"
+    githubLink: "https://github.com/SzymCode/ECommerceTemplate",
+    link: "https://e-commerce-template-szymcode.vercel.app/"
   },
   {
     title: "TodoApp",
     description:
       "This repository contains todo apps built with different technologies. Each folder represents a unique implementation, using different frameworks and programming languages.",
     listItem: ["Django", "React", "Redux", "Material UI"],
+    githubLink: "https://github.com/SzymCode/TodoApp",
     link: "https://github.com/SzymCode/TodoApp"
   },
   {
     title: "RegistrationDjango",
     description: "User registration and login template connected with database.",
     listItem: ["Django", "HTML5", "CSS3", "Bootstrap"],
+    githubLink: "https://github.com/SzymCode/RegistrationDjango",
     link: "https://github.com/SzymCode/RegistrationDjango"
   },
   {
     title: "TinyProjects",
     description: "Tiny projects that were made for educational purposes.",
     listItem: ["JavaScript", "TypeScript", "Python", "C++"],
+    githubLink: "https://github.com/SzymCode/TinyProjects",
     link: "https://github.com/SzymCode/TinyProjects"
   },
   {
     title: "RecruitmentTasks",
     description: "This repository contains my solutions for the recruitment tasks.",
     listItem: ["React", "Material UI", "TypeScript"],
+    githubLink: "https://github.com/SzymCode/RecruitmentTasks",
     link: "https://github.com/SzymCode/RecruitmentTasks"
+  },
+  {
+    title: "SzymCode/Portfolio",
+    description: "This project is showcase of my programming skills to potential employers or clients.",
+    listItem: ["React", "Tailwind", "Next.js"],
+    githubLink: "https://github.com/SzymCode/SzymCode/Portfolio",
+    link: "https://szymcode.vercel.app/"
   },
   {
     title: "ChessEngine",
     description: "Chess engine made with Pygame library + AI minimax algorithm.",
     listItem: ["Python", "Pygame"],
+    githubLink: "https://github.com/SzymCode/ChessEngine",
     link: "https://github.com/SzymCode/ChessEngine"
   },
   {
     title: "Snake",
     description: "Classic snake game built with Pygame library.",
     listItem: ["Python", "Pygame"],
+    githubLink: "https://github.com/SzymCode/Snake",
     link: "https://github.com/SzymCode/Snake"
   },
   {
     title: "ClipboardManager",
     description: "Small and simple multi-line clipboard manager.",
     listItem: ["Python", "Tkinter", "Pyperclip"],
+    githubLink: "https://github.com/SzymCode/ClipboardManager",
     link: "https://github.com/SzymCode/ClipboardManager"
   },
   {
     title: "YoutubeDownloader",
     description: "Simple youtube downloader made with tkinter. You can choose file extension .mp3, .mp4 or .3gpp.",
     listItem: ["Python", "Tkinter", "Pytube"],
+    githubLink: "https://github.com/SzymCode/YoutubeDownloader",
     link: "https://github.com/SzymCode/YoutubeDownloader"
   },
   {
-    title: "SzymCode/Portfolio",
-    description: "This project is showcase of my programming skills to potential employers or clients.",
-    listItem: ["React", "Tailwind", "Next.js"],
-    link: "https://github.com/SzymCode/SzymCode/Portfolio"
+    title: "DiscordClone",
+    description: "Not finished yet.",
+    listItem: ["React", "Tailwind", "Firebase", "Redux"],
+    githubLink: "https://github.com/SzymCode/SzymCode/DiscordClone",
+    link: "https://github.com/SzymCode/SzymCode/DiscordClone"
   },
 ]
--- portfolio/src/components/ArchiveCard.tsx
+++ portfolio/src/components/ArchiveCard.tsx
@@ -5,33 +5,37 @@ export interface ArchiveCardProps {
   title: string
   description: string
   listItem: string[]
+  githubLink: string
   link: string
 }
 
-export default function ArchiveCard({ title, description, listItem, link }: ArchiveCardProps) {
+export default function ArchiveCard({ title, description, listItem, githubLink, link }: ArchiveCardProps) {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
-      <div className="w-full h-68 md:h-76 rounded-lg p-6 bg-primary-lightblue dark:bg-primary-darker flex flex-col gap-6 hover:-translate-y-2 transition-transform duration-300 group">
-        <div className="flex justify-between items-center">
-          <FaRegFolder className="text-4xl text-primary" />
+    <div className="w-full h-full rounded-lg p-8 bg-primary-lightblue dark:bg-primary-darker flex flex-col gap-6 hover:-translate-y-2 transition-transform duration-300 group">
+      <div className="flex justify-between items-center mb-4">
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          <FaRegFolder className="text-4xl hover:text-primary dark:text-primary-lightgray dark:hover:text-primary duration-300 text-primary-black" />
+        </a>
+        <a href={link} target="_blank" rel="noopener noreferrer">
           <RxOpenInNewWindow className="text-4xl hover:text-primary dark:text-primary-lightgray dark:hover:text-primary duration-300" />
-        </div>
-        <div>
-          <h2 className="text-2xl font-semibold tracking-wide group-hover:text-primary -mt-1 duration-300">
-            {title}
-          </h2>
-          <p className="text-sm mt-4 mb-2 text-primary-black dark:text-primary-lightgray">
-            {description}
-          </p>
-        </div>
-        <ul className="text-xs mdl:text-sm text-primary-gray px-10 md:px-0 flex items-center text-center gap-4 mt-1">
-          {listItem.map((item, i) => (
-            <li key={i}>
-              {item}
-            </li>
-          ))}
-        </ul>
+        </a>
       </div>
-    </a>
+      <div>
+        <h2 className="text-2xl font-semibold tracking-wide group-hover:text-primary -mt-1 duration-300">
+          {title}
+        </h2>
+        <p className="text-sm mt-4 mb-2 text-primary-black dark:text-primary-lightgray">
+          {description}
+        </p>
+      </div>
+      <div className="flex-grow"></div>
+      <ul className="text-xs mdl:text-sm text-primary-gray px-10 md:px-0 flex items-center text-center gap-6 mt-1">
+        {listItem.map((item, i) => (
+          <li key={i}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
